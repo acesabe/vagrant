84db94a45dc3894d37d93147f66f29ba3061c35d
Acesabe <andys@bristolwireless.net>
2014-07-22 19:17:01 +0200

    Create preinstjrepuppet.sh
    dirty script to do what it says

---
+++ preinstjrepuppet.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+## Install Puppet
+
+sudo yum install -y puppet &&
+mkdir /vagrant/files/ && 
+cd /vagrant/files &&
+wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://s3-eu-west-1.amazonaws.com/acesabe-files/jre-8u5-linux-x64.rpm
+
+exit 0
