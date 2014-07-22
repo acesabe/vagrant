4cfedf0c2460e6b4da8ad716452e3b4ab4101708
Acesabe <andys@bristolwireless.net>
2014-07-22 19:28:28 +0200

    Update preinstjrepuppet.sh

--- preinstjrepuppet.sh
+++ preinstjrepuppet.sh
@@ -3,7 +3,7 @@
 ## Install Puppet
 
 sudo yum install -y puppet &&
-mkdir /vagrant/files/ && 
+mkdir -p /vagrant/files/ && 
 cd /vagrant/files &&
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" https://s3-eu-west-1.amazonaws.com/acesabe-files/jre-8u5-linux-x64.rpm
 
