2702b0945dd99ccb20d568140eee2d74a2aa8585
Acesabe <andys@bristolwireless.net>
2014-07-23 10:47:51 +0200

    site.pp
    only installs from local .rpm as proof of concept more than sensible use of Puppet!

---
+++ site.pp
@@ -0,0 +1,6 @@
+package  { 'jre-8u5-linux-x64.rpm':
+    provider => 'rpm',
+    source => '/vagrant/files/jre-8u5-linux-x64.rpm'
+}
+
+## to do: run alternatives to choose newly installed jre version
